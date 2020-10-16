1651358b4651207ca801bc0baf2383129a51ae60
HossamElharmeil <hossam.elharmil@gmail.com>
2020-10-16 03:04:19 +0200

    no semis

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -17,11 +17,11 @@
   // byeSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
   byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
+    console.log(speakWord + " " + name)
   }
   
   // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
   // 'byeSpeaker' on the global scope as well.
-  window.byeSpeaker = byeSpeaker;
+  window.byeSpeaker = byeSpeaker
   
 })(window)
